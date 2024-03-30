89f67da0f31c0c55671c0103940bdb5693313a56
ildartaipov <ildartaipov@icloud.com>
2024-03-30 21:31:41 +0500

    first commit

---
+++ arduino/radsens/radsens.ino
@@ -0,0 +1,159 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "CG_RadSens.h"   //ClimateGuard Radsense https://github.com/climateguard/RadSens
+#include "ArduinoJson.h"  //https://arduinojson.org/?utm_source=meta&utm_medium=library.properties
+
+#define VERSION "0.1"
+
+#define DEFAULT_TIMEOUT 300
+#define DEFAULT_HOST_TRANSMIT_INTERVAL 1000
+
+
+int hostTimeout = DEFAULT_TIMEOUT;
+int hostTransmitInterval = DEFAULT_HOST_TRANSMIT_INTERVAL;
+
+
+unsigned long timeNow;
+
+unsigned long hostTimeRecivie = 0;
+unsigned long hostTimeSend = 0;
+
+
+//SDA pin4 on rp2040 zero
+//SDl pin5 on rp2040 zero
+
+CG_RadSens radSens(RS_DEFAULT_I2C_ADDRESS); /*Constructor of the class ClimateGuard_RadSens1v2,
+                                                           sets the address parameter of I2C sensor.
+                                                           Default address: 0x66.*/
+
+int getHostTimeout(void) {
+  return hostTimeout;
+}
+
+int setHostTimeout(int _timeout) {
+  hostTimeout = _timeout;
+  return hostTimeout;
+}
+
+int getHostReceive(void) {
+  return hostTimeout;
+}
+
+int setHostTransmitInterval(int _interval) {
+  hostTransmitInterval = _interval;
+  return hostTransmitInterval;
+}
+
+int getHostTransmitInterval(void) {
+  return hostTransmitInterval;
+}
+
+// json буфер для пакетов от orange pi
+StaticJsonDocument<1024> jsondoc;
+DynamicJsonDocument answer(1024);
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();  // This function initializes the Wire library
+  delay(1000);
+  while (!radSens.init()) /*Initializates function and sensor connection. Returns false if the sensor is not connected to the I2C bus.*/
+  {
+    Serial.println("Sensor wiring error!");
+    delay(1000);
+  }
+
+
+  answer["name"] = "Rover radsense shield";
+  answer["version"] = VERSION;
+  serializeJson(answer, Serial);
+}
+
+void sendStatus() {
+
+  answer["status"]["radsens"]["intensyStatic"] = radSens.getRadIntensyStatic();
+  answer["status"]["radsens"]["intensyDynamic"] = radSens.getRadIntensyDynamic();
+  answer["status"]["radsens"]["sensorChipId"] = "0x" + String(radSens.getChipId(), HEX);
+  answer["status"]["radsens"]["firwareVersion"] = radSens.getFirmwareVersion();
+  answer["status"]["radsens"]["pulseCount"] = radSens.getNumberOfPulses();
+  // answer["status"]["radsens"]["adressI2C"] = "0x"+String(radSens.getSensorAddress(), HEX);
+  answer["status"]["radsens"]["HVGeneratorState"] = radSens.getHVGeneratorState();
+  answer["status"]["radsens"]["sensivity"] = radSens.getSensitivity();
+  answer["status"]["radsens"]["ledState"] = radSens.getLedState();
+  answer["status"]["radsens"]["time"] = millis();
+}
+
+void recieveDataFromHost(void) {
+  if (Serial.available()) {
+    DeserializationError err = deserializeJson(jsondoc, Serial);  // получаем сообщение от orange pi через uart
+    if (err == DeserializationError::Ok)                          // если cообщение принято
+    {
+
+      hostTimeRecivie = timeNow + getHostTimeout();
+      // JsonObject root = jsondoc.to<JsonObject>();
+      if (jsondoc["configure"]) {
+#if (SERIAL_DEBUG == 1)
+        Serial.println("Configure received!");
+#endif
+        String setLedState, setHVGeneratorState, setSensitivity;
+        setLedState = jsondoc["configure"]["setLed"].as<String>();
+        setHVGeneratorState = jsondoc["configure"]["setHVGeneratorState"].as<String>();
+        setSensitivity = jsondoc["configure"]["setSensitivity"].as<String>();
+        if (!setLedState.equalsIgnoreCase("null")) {
+          if (setLedState.equalsIgnoreCase("true")) {
+            radSens.setLedState(true);
+#if (SERIAL_DEBUG == 1)
+            Serial.println("Led ON");
+#endif
+          } else if (setLedState.equalsIgnoreCase("false")) {
+            radSens.setLedState(false);
+#if (SERIAL_DEBUG == 1)
+            Serial.println("Led OFF");
+#endif
+          }
+        }
+        if (!setHVGeneratorState.equalsIgnoreCase("null")) {
+          if (setHVGeneratorState.equalsIgnoreCase("true")) {
+            radSens.setHVGeneratorState(true);
+#if (SERIAL_DEBUG == 1)
+            Serial.println("HVGen ON");
+#endif
+          } else if (setHVGeneratorState.equalsIgnoreCase("false")) {
+            radSens.setHVGeneratorState(false);
+#if (SERIAL_DEBUG == 1)
+            Serial.println("HVGen OFF");
+#endif
+          }
+        }
+        if (!setSensitivity.equalsIgnoreCase("null")) {
+          uint16_t sensitivity = jsondoc["configure"]["setSensitivity"].as<int>();
+          radSens.setSensitivity(sensitivity);
+#if (SERIAL_DEBUG == 1)
+          Serial.print("Set sensivity to ");
+          Serial.println(sensitivity);
+#endif
+        }
+      }
+    } else {
+      while (Serial.available() > 0)
+        Serial.read();  // чистим буфер
+    }
+  }
+}
+
+void sendDataToHost(void) {
+  if (hostTimeSend > timeNow)
+    return;
+  hostTimeSend = timeNow + getHostTransmitInterval();
+  sendStatus();
+  serializeJson(answer, Serial);
+  Serial.println("");
+}
+void loop() {
+
+  timeNow = millis();
+  recieveDataFromHost();  // получаем данные от хоста
+  sendDataToHost();       // отправляем данные хосту c интервалом hostTransmitInterval
+  // showInfoOnDisplay();
+
+  delay(2000);
+}
---
+++ main.py
@@ -0,0 +1,170 @@
+from flask import Flask, render_template, Response, request
+import cv2
+import serial
+import threading
+import time
+import json
+import argparse
+
+def is_json(myjson):
+  try:
+    json.loads(myjson)
+  except ValueError as e:
+    return False
+  return True
+
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+led_port = "COM13"
+
+controlX, controlY = 0, 0  # глобальные переменные положения джойстика с web-страницы
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+video = cv2.VideoCapture(0)
+
+msg_led = {
+    "red": 0,
+    "green": 0,
+    "blue": 0
+}
+msg_chassis = {
+   "speed_l": 0,
+    "speed_r": 0
+}
+chassis_answer = {}
+radsens_answer = {}
+
+maxAbsSpeed = 255
+speedScale = 1
+def getFramesGenerator():
+    """ Генератор фреймов для вывода в веб-страницу, тут же можно поиграть с openCV"""
+    while True:
+        # time.sleep(0.01)  # ограничение fps (если видео тупит, можно убрать)
+        success, frame = camera.read()  # Получаем фрейм с камеры
+
+        if success:
+            frame = cv2.flip(frame, 1)
+            frame = cv2.resize(frame, (320, 240),
+                               interpolation=cv2.INTER_AREA)  # уменьшаем разрешение кадров (если видео тупит, можно уменьшить еще больше)
+            height, width = frame.shape[0:2]  # получаем разрешение кадра
+            # faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=10)
+
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # переводим кадр из RGB в HSV
+
+            # binary = cv2.inRange(hsv, (18, 60, 100), (32, 255, 255))  # пороговая обработка кадра (выделяем все желтое)
+            #binary = cv2.inRange(hsv, (0, 0, 0), (255, 255, 35))  # пороговая обработка кадра (выделяем все черное)
+
+            bin1 = cv2.inRange(hsv, (0, 60, 70), (10, 255, 255)) # красный цвет с одного конца
+            bin2 = cv2.inRange(hsv, (160, 60, 70), (179, 255, 255)) # красный цвет с другого конца
+            binary = bin1 + bin2  # складываем битовые маски
+
+
+            contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL,                               cv2.CHAIN_APPROX_NONE)  # получаем контуры выделенных областей
+
+            if len(contours) != 0:  # если найден хоть один контур
+                maxc = max(contours, key=cv2.contourArea)  # находим наибольший контур
+                moments = cv2.moments(maxc)  # получаем моменты этого контура
+                """
+                # moments["m00"] - нулевой момент соответствует площади контура в пикселях,
+                # поэтому, если в битовой маске присутствуют шумы, можно вместо
+                # if moments["m00"] != 0:  # использовать
+
+                if moments["m00"] > 20: # тогда контуры с площадью меньше 20 пикселей не будут учитываться 
+                """
+                if moments["m00"] > 20:  # контуры с площадью меньше 20 пикселей не будут учитываться
+                    cx = int(moments["m10"] / moments["m00"])  # находим координаты центра контура по x
+                    cy = int(moments["m01"] / moments["m00"])  # находим координаты центра контура по y
+
+                    iSee = True  # устанавливаем флаг, что контур найден
+
+                    controlX = 2 * (cx - width / 2) / width  # находим отклонение найденного объекта от центра кадра и
+                    # нормализуем его (приводим к диапазону [-1; 1])
+
+                    cv2.drawContours(frame, maxc, -1, (0, 255, 0), 1)  # рисуем контур
+                    cv2.line(frame, (cx, 0), (cx, height), (0, 255, 0), 1)  # рисуем линию линию по x
+                    cv2.line(frame, (0, cy), (width, cy), (0, 255, 0), 1)  # линия по y
+
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   # перевод изображения в градации серого
+           # _, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)  # бинаризуем изображение
+           #   print(len(faces))
+           #
+           #  if len(faces) > 1:
+           #      msg_led["red"], msg_led["green"], msg_led["blue"] = 0, 0, 25
+           #  elif len(faces) > 0:
+           #      msg_led["red"], msg_led["green"], msg_led["blue"] = 25, 0, 0
+           #  else:
+           #      msg_led["red"], msg_led["green"], msg_led["blue"] = 0, 25, 0
+
+            _, buffer = cv2.imencode('.jpg', frame)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """ Генерируем и отправляем изображения с камеры"""
+    return Response(getFramesGenerator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    """ Крутим html страницу """
+    return render_template('index.html')
+
+@app.route('/control')
+def control():
+    """ Пришел запрос на управления роботом """
+    global controlX, controlY
+    controlX, controlY = float(request.args.get('x')) / 100.0, float(request.args.get('y')) / 100.0
+    return '', 200, {'Content-Type': 'text/plain'}
+
+
+@app.route('/data')
+def data():
+    """ Пришел запрос на управления роботом """
+    global radsens_answer, chassis_answer
+    _data = radsens_answer
+    return _data, 200, {'Content-Type': 'application/json'}
+
+if __name__ == '__main__':
+
+    sendFreq = 10  # слать 10 пакетов в секунду
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, default=5000, help="Running port")
+    parser.add_argument("-i", "--ip", type=str, default='0.0.0.0', help="Ip address")
+    parser.add_argument('-cs', '--chassisserial', type=str, default='COM14', help="Chassis serial port")
+    parser.add_argument('-rs', '--radsensserial', type=str, default='COM11', help="Radsens serial port")
+    args = parser.parse_args()
+
+    chassisSerialPort = serial.Serial(args.chassisserial, 115200)
+    radsensSerialPort = serial.Serial(args.radsensserial, 115200)
+
+    def sender():
+        """ функция цикличной отправки пакетов по uart """
+        global msg_chassis, chassis_answer, radsens_answer
+        while True:
+            speed_l = maxAbsSpeed * (controlY + controlX)  # преобразуем скорость робота,
+            speed_r = maxAbsSpeed * (controlY - controlX)  # в зависимости от положения джойстика
+
+            speed_l = max(-maxAbsSpeed, min(speed_l, maxAbsSpeed))  # функция аналогичная constrain в arduino
+            speed_r = max(-maxAbsSpeed, min(speed_r, maxAbsSpeed))  # функция аналогичная constrain в arduino
+
+            msg_chassis["speed_l"], msg_chassis["speed_r"] = int(speedScale * speed_l), int(speedScale * speed_r)  # урезаем скорость и упаковываем
+            print("x, y:", controlX, controlY)
+
+            chassisSerialPort.write(json.dumps(msg_chassis, ensure_ascii=False).encode("utf8"))  # отправляем пакет в виде json файла
+            print(json.dumps(msg_chassis, ensure_ascii=False).encode("utf8"))
+
+            _answer = chassisSerialPort.readline()
+            if is_json(_answer):
+                chassis_answer = _answer
+
+            _answer = radsensSerialPort.readline()
+            if is_json(_answer):
+                radsens_answer = _answer
+            time.sleep(1 / sendFreq)
+
+threading.Thread(target=sender, daemon=True).start()  # запускаем тред отправки пакетов по uart с демоном
+
+app.run(debug=False, host=args.ip, port=args.port)  # запускаем flask приложение
